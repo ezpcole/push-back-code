14c0dd907b467bc741d3c3ff0da4740db560cb63
Eza <ezpcole@gmail.com>
2025-05-15 12:18:16 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,39 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       Edward                                                    */
+/*    Author:       Eza                                                       */
 /*    Created:      5/8/2025, 11:30:07 PM                                     */
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
+#include "orange.hpp"
 #include "vex.h"
-#include "orange.h"
 
-using namespace vex;
-
-competition Competition;
-vex::motor_group left(vex::motor(1),vex::motor(2),vex::motor(3));
-vex::motor_group right(vex::motor(4),vex::motor(5),vex::motor(6));
+vex::competition Competition;
+vex::motor motor1(1);
+vex::motor motor2(2, true);
+vex::motor motor3(3);
+vex::motor motor4(4);
+vex::motor motor5(5);
+vex::motor motor6(6);
+vex::motor_group left(motor1, motor2, motor3);
+vex::motor_group right(motor4, motor5, motor6);
 vex::controller Controller = vex::controller();
-orange::Drivetrain drivetrain(left,right);
+// orange::Drivetrain drivetrain(orange::BLUE, left, right);
 
-void pre_auton(void) {
+void pre_auton(void) {}
 
-}
-
-void autonomous(void) {
-
-}
+void autonomous(void) {}
 
 void usercontrol(void) {
+  left.setMaxTorque(100, vex::percent);
   while (1) {
-    wait(20, msec);
+    double leftController = Controller.Axis3.position();
+    int rightController = Controller.Axis2.position();
+    left.spin(vex::forward, 600 * (leftController / 100.0), vex::rpm);
+    // right.spin(vex::forward, 600 * (rightController / 100), vex::rpm);
+    vex::wait(20, vex::msec);
   }
 }
 
@@ -39,12 +44,7 @@ int main() {
   pre_auton();
 
   while (true) {
-    int leftController = Controller.Axis3.position();
-    int rightController = Controller.Axis2.position();
-    
-    left.spin(vex::forward,600*(leftController/100));
-    right.spin(vex::forward,600*(rightController/100));
 
-    wait(10, msec);
+    vex::wait(10, vex::msec);
   }
 }
