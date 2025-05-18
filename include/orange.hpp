256676faa243e5cdd5b2fe644b7e4948e404fe89
Eza <ezpcole@gmail.com>
2025-05-18 13:55:07 -0700

    Update orange.hpp

--- include/orange.hpp
+++ include/orange.hpp
@@ -4,6 +4,7 @@
 #include <cstdarg>
 #include <cstdio>
 #include <cstdlib>
+#include <functional>
 #include <string>
 #include <vector>
 
@@ -19,30 +20,36 @@ enum driveType { ROTATIONS_PER_MINUTE, DEGREES_PER_SECOND, PERCENTAGE };
 class MotorGroup {
 private:
   std::vector<vex::motor> motors;
+  orange::gearboxType gearbox;
 
 public:
-  MotorGroup(...);
+  MotorGroup(orange::gearboxType gearbox, std::vector<vex::motor> motors);
   std::vector<vex::motor> GetMotors();
-  void drive();
+  void drive(bool forward, double units, orange::driveType driveType);
 };
 
 class Drivetrain {
 private:
-  vex::motor_group left;
-  vex::motor_group right;
-  vex::rotation odom;
+  orange::MotorGroup left;
+  orange::MotorGroup right;
+  // vex::rotation odom;
   double wheelTravel;
   double trackWidth;
   double wheelBase;
   double gearRatio;
   orange::gearboxType gearbox;
+  double ki;
+  double kp;
+  double kd;
+  double previousError;
+  double integral;
 
 public:
   void forward(float_t inches);
   void forward_async(float_t inches);
   void turn(float_t deg);
   void turn_async(float_t deg);
-  void drive(bool forward, orange::driveType driveType);
+  void drive(bool forward, double units, orange::driveType driveType);
   void brake(vex::brakeType mode);
 
   double GetWheelTravel();
@@ -64,6 +71,23 @@ public:
   //   Drivetrain(orange::gearboxType gearbox, vex::motor_group left,
   //              vex::motor_group right, vex::rotation odom);
   //   Drivetrain();
+};
+
+struct point {
+  double x;
+  double y;
+};
+
+class graph {
+public:
+  static void drawGraph(double minX, double maxX, double minY, double maxY,
+                        std::vector<orange::point> points);
+  static double convertPoint(double n, double min, double max);
+  static int windowOffsetX;
+  static int windowOffsetY;
+  static int windowEndX;
+  static int windowEndY;
+  static int hue;
 };
 } // namespace orange
 
