533fbb4b48046c0d28cf19953c2cbf8c3ebfca3e
Eza <ezpcole@gmail.com>
2025-05-18 13:54:25 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,15 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
+#include "graph.hpp"
 #include "orange.hpp"
 #include "vex.h"
+#include <iostream>
+#include <string>
+#include <vector>
 
 vex::competition Competition;
+vex::brain Brain;
 vex::motor motor1(1);
 vex::motor motor2(2, true);
 vex::motor motor3(3);
@@ -27,12 +32,26 @@ void pre_auton(void) {}
 void autonomous(void) {}
 
 void usercontrol(void) {
+  // Brain.Screen.clearScreen();
+
+  std::vector<orange::point> examplePoints;
+  examplePoints.push_back({0, 0});
+
   left.setMaxTorque(100, vex::percent);
+
+  double count = 0;
   while (1) {
+
     double leftController = Controller.Axis3.position();
     int rightController = Controller.Axis2.position();
     left.spin(vex::forward, 600 * (leftController / 100.0), vex::rpm);
     // right.spin(vex::forward, 600 * (rightController / 100), vex::rpm);
+
+    Brain.Screen.clearScreen();
+    count += 10;
+    examplePoints.push_back({count, 1.5 * count});
+    // orange::graph::drawGraph(0, 100, 0, 100, examplePoints);
+
     vex::wait(20, vex::msec);
   }
 }
@@ -43,8 +62,5 @@ int main() {
 
   pre_auton();
 
-  while (true) {
-
-    vex::wait(10, vex::msec);
-  }
+  // orange::graph::drawGraph(0, 100, 0, 100, examplePoints);
 }
